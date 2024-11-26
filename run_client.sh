52166724ccf733fd73b19d3545748f4d691ee556
Rabia <rabianaeem1998@gmail.com>
2024-11-26 14:03:36 +0500

    working fine

--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,3 @@
 
-python training_fl.py --server localhost:8080 --seed $RANDOM  --folder data --early-stop 50 --normalisation ln --num-clients 4 --partition $1 --diffusion --diffusion-folder "run_nonIID_fl_protein_4" &
+python training_fl.py --server localhost:8080 --seed $RANDOM  --folder data --early-stop 50 --normalisation ln --num-clients 4 --partition $1 --diffusion --diffusion-folder "run_nonIID_fl_protein_4" 
 #  export TF_ENABLE_ONEDNN_OPTS=0
--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 
-python server.py --num-rounds 10 --num-clients 4 --folder data --seed $RANDOM --early-stop 50 --save-name "model_nonIID_fl_protein_4" --normalisation ln &> output_nonIID_fl_protein_4 &
+python server.py --num-rounds 10 --num-clients 4 --folder data --seed $RANDOM --early-stop 50 --save-name "model_nonIID_fl_protein_4" --normalisation ln &> output_nonIID_fl_protein_4 
 #  export TF_ENABLE_ONEDNN_OPTS=0
